996cd6560eb0d1ccf9acddce13c5216d63fa020b
RajanRathod7725 <65702470+RajanRathod7725@users.noreply.github.com>
2020-05-21 13:04:14 +0530

    Update Ajax_activate_account.php

--- application/controllers/admin/Ajax_activate_account.php
+++ application/controllers/admin/Ajax_activate_account.php
@@ -2,6 +2,7 @@
 class Ajax_activate_account extends CI_Controller {
 	public function __construct()
     {
+		/**change**///
 		parent::__construct();
 		$this->load->model('admin/admin_model');
 		$this->load->model('database_model');
